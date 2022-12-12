bb683275dccd468163eca9ae432fa4c5d03c46d3
Rafał Łazicki <rafal.lazicki99@gmail.com>
2022-12-12 09:37:34 +0100

    [IMP] Restrict model views capabilities

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,16 +1,14 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import GenericViewSet, mixins
 
 from tickets.models import Ticket, TicketSale
 from tickets.serializers import TicketSaleSerializer, TicketSerializer
 
-# Create your views here.
 
-
-class TicketViewSet(viewsets.ModelViewSet):
+class TicketViewSet(GenericViewSet, mixins.ListModelMixin):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
 
-class TicketSaleViewSet(viewsets.ModelViewSet):
+class TicketSaleViewSet(GenericViewSet, mixins.CreateModelMixin):
     queryset = TicketSale.objects.all()
     serializer_class = TicketSaleSerializer
